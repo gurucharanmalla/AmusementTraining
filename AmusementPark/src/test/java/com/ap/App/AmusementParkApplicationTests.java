10e014e62e0fe36f7dd6bfbed4d408e8c33824a1
gurucharanmalla <180070171me@gmail.com>
2022-07-13 15:42:45 +0530

    s1

---
+++ AmusementPark/src/test/java/com/ap/App/AmusementParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmusementParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
