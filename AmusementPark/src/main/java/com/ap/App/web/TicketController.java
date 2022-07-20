58d8d1bc1bd3cedf75912c2a0c807724d01ec8be
nagoorvalishaik <nagoorshaik817@gmail.com>
2022-07-20 15:22:14 +0530

    web controller

--- AmusementPark/src/main/java/com/ap/App/web/TicketController.java
+++ AmusementPark/src/main/java/com/ap/App/web/TicketController.java
@@ -1,10 +1,15 @@
 package com.ap.App.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +29,43 @@ public class TicketController {
 	{
 		return ticketservice.getTicketDetails(ticketId);
 	}
+	
+	
+	@PostMapping("/save")
+	private Ticket  insertTicket(@RequestBody Ticket t)
+	{
+		return ticketservice.insertTicketBooking(t);
+	}
+	
+	@PutMapping("/update")
+	private boolean updateTicket(@RequestBody Ticket t) {
+		
+		ticketservice.updateTicketBooking(t);
+		return true;
+		
+	}
+	
+	@GetMapping("/tickets/{customerId}")
+	private List<Ticket> viewAllTIckets(@PathVariable int customerId){
+		
+		return ticketservice.viewAllTickets(customerId); 
+	}
+	
+	@DeleteMapping("/{ticketId}")
+	private void deleteTicket(@PathVariable int ticketId) {
+		
+		 ticketservice.deleteTicketBooking(ticketId);
+	}
+	
+	@GetMapping("/{ticketId}/bill")
+	private double getBill(@PathVariable int ticketId) {
+		return ticketservice.calculateBill(ticketId);
+	}
+	
+	
+	
+	
+	
+	
+	
 }
